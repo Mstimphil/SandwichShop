45bc13a108db31b4b37d4eadb52e03ce9bed49f2
Mstimphil <mstimphil@my.yearupunited.org>
2025-09-25 14:33:07 -0400

    ages commit

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,11 +1,27 @@
 package com.pluralsight;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner myScanner =new Scanner(System.in);
         System.out.print("What is the size of the Sandwich? ");
-
+        String command = myScanner.nextLine();
+        boolean equals = equals(5.45);
+        if (boolean equals small or large);
+        small == 5.45;
+        large== 8.95
     }
 }
+String name = "Monalisa";
+int age 28
+double price;
+if (age <= 17 ) {
+    price = price * .10
+        }
+System.out.println("10% Discount");
+else (age >= 65 ) {
+    price = price * .20
+        }
+System.out.println("20% Discount")
